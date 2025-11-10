581345c036dcb9c5295e88113c1beae18486ac35
Ice <Tonasamya@gmail.com>
2025-11-10 07:30:40 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/Portfolio/', // <--- ADD THIS LINE HERE!
+  base: '/', // <--- ADD THIS LINE HERE!
   server: {
     host: "::",
     port: 8080,
